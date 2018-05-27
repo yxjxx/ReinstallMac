86d0f96d50fe6915a17a3ed54c47df88ef2410cd
rico <ricoyangjing@didichuxing.com>
2018-05-27 18:59:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 全新安装 macOS 之后
+
+[My Must-Have Mac Apps 2017](https://www.yxjxx.com/my_must_have_mac_apps_2017.html)
---
+++ mas_installs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mas install 445189367 # PopClip (1.5.8)
+mas install 403504866 # PCalc (4.5.6)
+mas install 409183694 # Keynote (8.0.1)
+mas install 529490330 # LilyView (1.2.3)
+mas install 1101244278 # iPic - Image Upload Tool (1.5.4)
+mas install 457622435 # Yoink - Improved Drag and Drop (3.4.3)
+mas install 890031187 # Marked 2 (2.5.16)
+mas install 967805235 # Paste 2 (2.3.6)
+mas install 937984704 # Amphetamine (3.0.2)
+mas install 918207447 # Annotate - Capture and Share (2.1.4)
+mas install 419330170 # Moom (3.2.11)
+mas install 836500024 # WeChat (2.3.13)
